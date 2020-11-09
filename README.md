fa0faa48df85e1521b679febadbcd6428cff5df3
Jan Hagelauer <7543325+jan-san@users.noreply.github.com>
2020-11-09 14:06:19 +0000

    feat: implement action, add documentation

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# graphile-migrate-commit-current-action
-Commit Graphile Migrations in a GitHub Action
+# Commit current Graphile Migrate migration
+
+This action commits the current migration. Migrations must be present under `/migrations/`.
+
+Use this action in conjunction with [Add & Commit](https://github.com/marketplace/actions/add-commit) to commit
+the committed migration to the repository.
+
+## Inputs
+
+none
+
+## Outputs
+
+none
+
+## Example usage
+
+```yaml
+name: Commit current migration (test)
+
+on:
+  [ push ]
+
+jobs:
+  commit-current-migration:
+    runs-on: ubuntu-latest
+
+    steps:
+    - uses: actions/checkout@v2
+
+    - name: Commit current migration
+      uses: actions/graphile-migrate-commit-current-action@v1.0.0
+
+    - uses: EndBug/add-and-commit@v5
+       with:
+         add: migrations/current.sql migrations/committed/*.sql
+         message: Commit migration changes in repo
+```
---
+++ src/commitMigration.js
@@ -0,0 +1,79 @@
+const fs = require('fs')
+const { parseMigrationText, getAllMigrations, serializeMigration } = require('graphile-migrate/dist/migration')
+const { readCurrentMigration, getCurrentMigrationLocation } = require('graphile-migrate/dist/current')
+const { sluggify } = require('graphile-migrate/dist/sluggify')
+const { calculateHash }  = require('graphile-migrate/dist/hash')
+const omit = require('lodash/omit')
+
+const migrationsFolder = './migrations'
+const currentMigrationPath = `${migrationsFolder}/current.sql`
+const pgmSettings = { migrationsFolder }
+
+module.exports = async function() {
+  try {
+    await fs.promises.access(
+      `${migrationsFolder}/committed`,
+      fs.constants.R_OK
+    )
+  } catch (e) {
+    console.error(`Failed to open migrations directory: ${e.message}`)
+    return;
+  }
+  const committedMigrations = await getAllMigrations(pgmSettings)
+  const lastCommittedMigration = committedMigrations[committedMigrations.length - 1]
+  console.log(`Last committed migration: ${lastCommittedMigration.realFilename}`)
+
+  const newMigrationNumber = lastCommittedMigration
+    ? parseInt(lastCommittedMigration.filename, 10) + 1
+    : 1;
+  
+  const currentLocation = await getCurrentMigrationLocation(pgmSettings);
+  const currentMigrationContent = await readCurrentMigration(pgmSettings, currentLocation);
+
+  const { headers: currentMigrationHeaders, body: currentMigrationBody } = parseMigrationText(
+    currentMigrationPath,
+    currentMigrationContent,
+    false
+  )
+  const currentMigrationMessage = currentMigrationHeaders.Message
+
+  if (currentMigrationBody.trim() === '') {
+    console.log('Current migration is empty - skipping commit.')
+    return
+  }
+
+  if (currentMigrationMessage && /[\r\n\0\b\v\f\cA-\cZ]/u.test(currentMigrationMessage)) {
+    throw new Error("Invalid commit message: contains disallowed characters");
+  }
+  if (currentMigrationMessage && currentMigrationMessage.length > 512) {
+    throw new Error(
+      "Invalid commit message: message is too long (max: 512 chars)",
+    );
+  }
+
+  const newMigrationFileName = `${
+    String(newMigrationNumber).padStart(6, '0')
+  }${
+    currentMigrationMessage ? `-${sluggify(currentMigrationMessage)}` : '' 
+  }.sql`
+  console.log(`New migration name: ${newMigrationFileName}`)
+
+  const newMigrationHash = calculateHash(
+    currentMigrationBody,
+    lastCommittedMigration && lastCommittedMigration.hash
+  )
+
+  const newMigrationContent = serializeMigration(
+    currentMigrationBody,
+    {
+      Previous: lastCommittedMigration ? lastCommittedMigration.hash : "-",
+    Hash: newMigrationHash,
+    Message: currentMigrationMessage || undefined,
+    ...omit(currentMigrationHeaders, ["Previous", "Hash", "Message"]),
+    }
+  )
+  fs.writeFileSync(
+    `${migrationsFolder}/committed/${newMigrationFileName}`,
+    newMigrationContent
+  )
+}
